09515089f3ae86207f300114fd3e2660b7f11432
aletisusmita <102968066+aletisusmita@users.noreply.github.com>
2022-11-01 14:29:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-// import chooseHall from "./component/Basics/chooseHall";
+
 import Moviebar from "./component/Basics/moviebar";
-// import Seatbooking from "./component/Basics/seatBooking";
-// import UseState from "./component/Hooks/useState";
-// import UseEffect from "./component/Hooks/useEffect";
-// import UseReducer from "./component/Hooks/useReducer";
+
 
 const App = () => {
-  // return <UseReducer />;
+ 
  return <Moviebar />
-// return <Seatbooking />
-//  return <chooseHall />
+
 };
 
 export default App;
